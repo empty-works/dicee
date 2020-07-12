53b57d3e55d34d28feb4757d5f1666272b7d0522
empty-works <pama.mark@gmail.com>
2020-07-12 16:24:32 +0200

    Refactored javascript code to avoid needing a cumbersome function.

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,31 +1,9 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-
-function getDice(randNumber) {
-
-	var fileName;
-	if(randNumber == 1) {
-		fileName = "images/dice1.png";
-	}
-	else if(randNumber == 2) {
-		fileName = "images/dice2.png";
-	}
-	else if(randNumber == 3) {
-		fileName = "images/dice3.png"; 
-	}
-	else if(randNumber == 4) {
-		fileName = "images/dice4.png";
-	}
-	else if(randNumber == 5) {
-		fileName = "images/dice5.png";
-	}
-	else if(randNumber == 6) {
-		fileName = "images/dice6.png";
-	}
-	return fileName;
-}
-document.querySelectorAll("img.img1")[0].setAttribute("src", getDice(randomNumber1));
-document.querySelectorAll("img.img2")[0].setAttribute("src", getDice(randomNumber2));
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img.img1")[0].setAttribute("src", randomDiceImage1);
+document.querySelectorAll("img.img2")[0].setAttribute("src", randomDiceImage2);
 
 if(randomNumber1 > randomNumber2) {
 	document.getElementById("first_h1").innerHTML = "Player 1 Wins!";
