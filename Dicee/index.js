14e0d878bfc423fdeed2257ef1fe5ab06cccef0a
empty-works <pama.mark@gmail.com>
2020-07-12 14:26:46 +0200

    Fixed issue with the left random dice not changing properly.

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,21 +1,21 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var fileName = "images/dice" + randomNumber1 + ".png";
+var fileName;
 if(randomNumber1 == 1) {
-	fileName = "dice1.png";
+	fileName = "images/dice1.png";
 }
 else if(randomNumber1 == 2) {
-	fileName = "dice2.png";
+	fileName = "images/dice2.png";
 }
 else if(randomNumber1 == 3) {
-	fileName = "dice3.png"; 
+	fileName = "images/dice3.png"; 
 }
 else if(randomNumber1 == 4) {
-	fileName = "dice4.png";
+	fileName = "images/dice4.png";
 }
 else if(randomNumber1 == 5) {
-	fileName = "dice5.png";
+	fileName = "images/dice5.png";
 }
 else if(randomNumber1 == 6) {
-	fileName = "dice6.png";
+	fileName = "images/dice6.png";
 }
 document.querySelectorAll("img.img1")[0].setAttribute("src", fileName);
