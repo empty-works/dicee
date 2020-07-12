015c5732186e465fbc29014c2fe7cfd2e01f2614
empty-works <pama.mark@gmail.com>
2020-07-12 14:42:24 +0200

    Created a function to encompass if/else statements and make them reusable for multiple dice.

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,21 +1,27 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var fileName;
-if(randomNumber1 == 1) {
-	fileName = "images/dice1.png";
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+function getDice(randNumber) {
+
+	var fileName;
+	if(randomNumber1 == 1) {
+		fileName = "images/dice1.png";
+	}
+	else if(randomNumber1 == 2) {
+		fileName = "images/dice2.png";
+	}
+	else if(randomNumber1 == 3) {
+		fileName = "images/dice3.png"; 
+	}
+	else if(randomNumber1 == 4) {
+		fileName = "images/dice4.png";
+	}
+	else if(randomNumber1 == 5) {
+		fileName = "images/dice5.png";
+	}
+	else if(randomNumber1 == 6) {
+		fileName = "images/dice6.png";
+	}
+	return fileName;
 }
-else if(randomNumber1 == 2) {
-	fileName = "images/dice2.png";
-}
-else if(randomNumber1 == 3) {
-	fileName = "images/dice3.png"; 
-}
-else if(randomNumber1 == 4) {
-	fileName = "images/dice4.png";
-}
-else if(randomNumber1 == 5) {
-	fileName = "images/dice5.png";
-}
-else if(randomNumber1 == 6) {
-	fileName = "images/dice6.png";
-}
-document.querySelectorAll("img.img1")[0].setAttribute("src", fileName);
+document.querySelectorAll("img.img1")[0].setAttribute("src", randomNumber1);
