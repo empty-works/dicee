9f8fee8420e63a7e4bec36963bc772d137d6af06
empty-works <pama.mark@gmail.com>
2020-07-12 14:47:38 +0200

    Made some corrections to the getDice function.

--- Dicee/index.js
+++ Dicee/index.js
@@ -4,24 +4,24 @@ var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 function getDice(randNumber) {
 
 	var fileName;
-	if(randomNumber1 == 1) {
+	if(randNumber == 1) {
 		fileName = "images/dice1.png";
 	}
-	else if(randomNumber1 == 2) {
+	else if(randNumber == 2) {
 		fileName = "images/dice2.png";
 	}
-	else if(randomNumber1 == 3) {
+	else if(randNumber == 3) {
 		fileName = "images/dice3.png"; 
 	}
-	else if(randomNumber1 == 4) {
+	else if(randNumber == 4) {
 		fileName = "images/dice4.png";
 	}
-	else if(randomNumber1 == 5) {
+	else if(randNumber == 5) {
 		fileName = "images/dice5.png";
 	}
-	else if(randomNumber1 == 6) {
+	else if(randNumber == 6) {
 		fileName = "images/dice6.png";
 	}
 	return fileName;
 }
-document.querySelectorAll("img.img1")[0].setAttribute("src", randomNumber1);
+document.querySelectorAll("img.img1")[0].setAttribute("src", getDice(randomNumber1));
