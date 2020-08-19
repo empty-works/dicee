39d78593f768e754e1efed71df4ea6314b4b3565
empty-works <pama.mark@gmail.com>
2020-08-19 03:22:59 +0200

    Using a for loop to iterate through all drum buttons and adding an event listener to them.

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,2 +1,6 @@
-document.querySelector("button").addEventListener("click", function () {alert("I've been clicked!"});
+var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOfDrumsButtons; i++) {
+	document.querySelector("button")[i].addEventListener("click", function () {alert("I've been clicked!"});
+}
 
