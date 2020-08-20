51334d1b0e3f1c17a8b937cbce2a13bc9b560931
empty-works <pama.mark@gmail.com>
2020-08-20 04:11:24 +0200

    Made syntax corrections to the function.

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,8 +1,8 @@
 var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < numberOfDrumsButtons; i++) {
-	document.querySelector("button")[i].addEventListener("click", function () {
-		var audio = Audio('tom-1.mp3');
+	document.querySelectorAll("button")[i].addEventListener("click", function () {
+		var audio = new Audio("sounds/tom-1.mp3");
 		audio.play();
 	});
 }
