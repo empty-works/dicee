ac54fb077491c011dea05cce91c9ec3d07c9e982
empty-works <pama.mark@gmail.com>
2020-08-20 04:12:29 +0200

    Fixed issue where the drum sound was not playing.

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,7 +1,7 @@
 var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < numberOfDrumsButtons; i++) {
-	document.querySelectorAll("button")[i].addEventListener("click", function () {
+	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 		var audio = new Audio("sounds/tom-1.mp3");
 		audio.play();
 	});
