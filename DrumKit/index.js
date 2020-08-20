4c4ae839f630e339480073c0beaa6ca6fb19e1cc
empty-works <pama.mark@gmail.com>
2020-08-20 04:00:49 +0200

    Added an Audio object to the listener function.

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,6 +1,9 @@
 var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < numberOfDrumsButtons; i++) {
-	document.querySelector("button")[i].addEventListener("click", function () {alert("I've been clicked!"});
+	document.querySelector("button")[i].addEventListener("click", function () {
+		var audio = Audio('tom-1.mp3');
+		audio.play();
+	});
 }
 
