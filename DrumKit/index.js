70c96001e8325d76b6b9f114277d3261737208da
empty-works <pama.mark@gmail.com>
2020-08-21 03:58:51 +0200

    Implemented all cases for the tom sound.

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -9,9 +9,24 @@ for(var i = 0; i < numberOfDrumsButtons; i++) {
 		switch(buttonInnerHTML) {
 			
 			case "w":
-				var audio = new Audio("sounds/tom-1.mp3");
-				audio.play();
-			break;
+				var tom1 = new Audio("sounds/tom-1.mp3");
+				tom1.play();
+				break;
+
+			case "a":
+				var tom2 = new Audio("sounds/tom-2.mp3");
+				tom2.play();
+				break;
+
+			case "s":
+				var tom3 = new Audio("sounds/tom-3.mp3");
+				tom3.play();
+				break;
+
+			case "d":
+				var tom4 = new Audio("sounds/tom-4.mp3");
+				tom4.play();
+				break;
 
 			default:
 		}
