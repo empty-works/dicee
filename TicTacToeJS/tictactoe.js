d8433cf86281a943931c4a1c29263d53ec2fc096
empty-works <pama.mark@gmail.com>
2020-09-17 21:42:52 +0200

    Added JS code to clear the squares.

--- TicTacToeJS/tictactoe.js
+++ TicTacToeJS/tictactoe.js
@@ -1,2 +1,18 @@
-var table_cell = document.querySelectorAll("td");
+// Restart the game
+var restart_button = document.querySelector("#b");
 
+// Grabs all the squares
+var table_cells = document.querySelectorAll("td");
+
+// Clear the squares
+function clear_cells() {
+	for(var i = 0; i < table_cells.length; i++) {
+		table_cells[i].textContent = '';	
+	}
+}
+
+restart_button.addEventListener("click", clear_cells);
+
+// Check the square marker
+
+// For loop to add event listeners to all the squares
