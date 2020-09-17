0e905f3c5b2608a50b3ebbe3c6227f8c03ff962f
empty-works <pama.mark@gmail.com>
2020-09-17 21:55:56 +0200

    Clicking on the squares now produces an X or an O or clears the square.

--- TicTacToeJS/tictactoe.js
+++ TicTacToeJS/tictactoe.js
@@ -13,6 +13,20 @@ function clear_cells() {
 
 restart_button.addEventListener("click", clear_cells);
 
-// Check the square marker
+// Check all the squares
+function changeMarker() {
+	if(this.textContent === '') {
+		this.textContent = 'X';
+	}
+	else if(this.textContent === 'X') {
+		this.textContent = 'O';
+	}
+	else {
+		this.textContent = '';
+	}
+}
 
 // For loop to add event listeners to all the squares
+for(var i = 0; i < table_cells.length; i++) {
+	table_cells[i].addEventListener("click", changeMarker);
+}
