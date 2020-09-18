679be6da324c1f754c4668bf370253b84c473811
empty-works <pama.mark@gmail.com>
2020-09-18 20:24:53 +0200

    Added functions that return the color of the selected chip and a function that checks the bottom of the board if there is a chip there.

--- ConnectFour/connectfour.js
+++ ConnectFour/connectfour.js
@@ -11,4 +11,16 @@ function changeColor(rowIndex, colIndex, color) {
 	return table.eq(rowIndex).find("td").eq(colIndex).find("button").css("background-color", color);
 }
 
+function returnColor(rowIndex, colIndex) {
+	return table.eq(rowIndex).find("td").eq(colIndex).find("button").css("background-color");
+}
 
+function checkBottom(colIndex) {
+	var colorInput = returnColor(5, colIndex);
+	for(var row = 5; row >= 0; row--) {
+		colorInput = returnColor(row, colIndex);
+		if(colorInput === "rgb(128, 128, 128)") {
+			return row;
+		}
+	}
+}
