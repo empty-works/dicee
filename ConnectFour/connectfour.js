92e48a50e9edab152d7833d0648a7e58a1cf1f13
empty-works <pama.mark@gmail.com>
2020-09-18 20:15:53 +0200

    JS code to prompt players to enter their names. Assign colors to the players. Function that enables the chip to change color (mostly from grey to blue or red).

--- ConnectFour/connectfour.js
+++ ConnectFour/connectfour.js
@@ -1,1 +1,14 @@
-// This is JS
+var player1 = prompt("Player 1, enter your name. You will be blue");
+var player1Color = "rgb(86, 151, 255)";
+
+var player2 = prompt("Player 2, enter your name. You will be red");
+var player2Color = "rgb(237, 45, 73)";
+
+var game_on = true;
+var table = $("table tr");
+
+function changeColor(rowIndex, colIndex, color) {
+	return table.eq(rowIndex).find("td").eq(colIndex).find("button").css("background-color", color);
+}
+
+
